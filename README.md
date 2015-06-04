d2180c37807bad7391331093aa30567cbd68c090
Eric Barbour <emb4gu@virginia.edu>
2015-06-04 15:51:18 -0400

    Improve install script and some preference stuff

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ JetBrains-ZenBurn
 =================
 
 Zenburn theme for JetBrains appCode and JetBrains PyCharm
+
+Forked from [darvin/JetBrains-ZenBurn](http://github.com/darvin/JetBrains-ZenBurn) and improved for more recent versions of JetBrains IDEs
--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-ln -sf `pwd`/ZenBurn.xml ${HOME}/Library/Preferences/PyCharm20/colors/ZenBurn.xml
+if [[ ! -d ${{HOME}/Library/Preferences/PyCharm40/colors}]]; then
+  `mkdir` ${{HOME}/Library/Preferences/PyCharm40/colors
+fi
+
+if [[ ! -d ${{HOME}/Library/Preferences/PyCharm40/colors}]]; then
+  `mkdir` ${{HOME}}/Library/Preferences/appCode10/colors
+fi
+
+# Finally make symlinks
+ln -sf `pwd`/ZenBurn.xml ${HOME}/Library/Preferences/PyCharm40/colors/ZenBurn.xml
 ln -sf `pwd`/ZenBurn.xml ${HOME}/Library/Preferences/appCode10/colors/ZenBurn.xml
